eca46f81e46809fede5c5e569600fe2f6ad7f3cf
rakib-hasan <mahmud1807077@stud.kuet.ac.bd>
2022-05-17 04:37:36 +0600

    practicing Merge commit1

---
+++ app/src/main/java/com/example/calculator/practicingMerge.java
@@ -0,0 +1,17 @@
+package com.example.calculator;
+
+public class practicingMerge {
+    int a;
+
+    public practicingMerge(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
